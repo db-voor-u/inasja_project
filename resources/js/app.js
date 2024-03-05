2e9d69a9760fe12f7cb565a74d9da1ac82174836
JeanPierre Meijer <duidelijkheidinbeeld@outlook.com>
2024-03-05 17:40:49 +0100

    app.js commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,2 @@
 import './bootstrap';
-import'./components/navigation';
 
-
-function toggleMenu() {
-    const menuContent = document.getElementById('menu-content');
-    const menuIcon = document.getElementById('menu-close-icon');
-
-    menuContent.classList.toggle('hidden');
-    menuIcon.style.display = menuIcon.style.display === 'none' ? 'block' : 'none';
-}
-
-window.toggleMenu = toggleMenu;
