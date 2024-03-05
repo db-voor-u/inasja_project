d68c407500fd0ad5fcfc77fd7d82dfb97336b68a
JeanPierre Meijer <duidelijkheidinbeeld@outlook.com>
2024-03-05 19:51:34 +0100

    aanpassingen build

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-
+ base: '/inasja_project/',
     plugins: [
+
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
