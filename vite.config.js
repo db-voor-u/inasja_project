4e7ba6d69aff0abb7172eb7559e9a3d1da4eae7f
JeanPierre Meijer <duidelijkheidinbeeld@outlook.com>
2024-03-06 12:24:57 +0100

    instellingen invoeren

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
 
         laravel({
-            input: ['resources/css/app-CKW-exbu.css', 'resources/js/app.js'],
+            input: ['resources/css/app-CKW-exbu.css', 'resources/js/app--CifqVuM1.js'],
             refresh: true,
         }),
     ],
